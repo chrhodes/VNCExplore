9b45e2702bc76d7d1c52816587f9040caa12c672
Christopher Rhodes <chrhodes@cox.net>
2024-07-10 14:31:44 -0700

    End of Lesson 2

---
+++ EFCore/EF8-Fundamentals-Julie Lerman/PublisherConsole/Program.cs
@@ -0,0 +1,97 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Linq;
+
+using Microsoft.EntityFrameworkCore;
+
+using PublisherData;
+
+using PublisherDomain;
+
+namespace PublisherConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            using (PubContext context = new PubContext())
+            {
+                // NOTE(crhodes)
+                // This is good for demos.  Just create on the fly
+
+                context.Database.EnsureCreated();
+            }
+
+            var p = new Program();
+
+            //p.GetAuthors();
+            //p.AddAuthors();
+            //p.GetAuthors();
+
+            p.AddAuthorWithBook();
+            p.GetAuthorsWithBooks();
+        }
+
+        private void GetAuthors()
+        {
+            //using (var context = new PubContext())
+            //{
+
+            //}
+
+            // TODO(crhodes)
+            // Learn about this syntax
+
+            using var context = new PubContext();
+
+            var authors = context.Authors.ToList();
+
+            foreach (var author in authors)
+            {
+                Console.WriteLine(author.FirstName + " " + author.LastName);
+            }
+        }
+        private void AddAuthors()
+        {
+            var author = new Author { FirstName = "Archer" , LastName = "Schrhodes"};
+
+            using var context = new PubContext();
+
+            context.Authors.Add(author);
+            context.SaveChanges();
+        }
+
+        void AddAuthorWithBook()
+        {
+            var author = new Author { FirstName = "Julie", LastName = "Lerman" };
+            author.Books.Add(new Book
+            {
+                Title = "Programming Entity Framework",
+                PublishDate = new DateOnly(2009, 1, 1)
+            });
+            author.Books.Add(new Book
+            {
+                Title = "Programming Entity Framework 2nd Ed",
+                PublishDate = new DateOnly(2010, 8, 1)
+            });
+            using var context = new PubContext();
+            context.Authors.Add(author);
+            context.SaveChanges();
+        }
+        void GetAuthorsWithBooks()
+        {
+            using var context = new PubContext();
+            var authors = context.Authors.Include(a => a.Books).ToList();
+            foreach (var author in authors)
+            {
+                Console.WriteLine(author.FirstName + " " + author.LastName);
+                foreach (var book in author.Books)
+                {
+                    Console.WriteLine(book.Title);
+                }
+            }
+        }
+    }
+}
---
+++ EFCore/EF8-Fundamentals-Julie Lerman/PublisherData/PubContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+using PublisherDomain;
+
+namespace PublisherData
+{
+    public class PubContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = PubDatabase");
+
+            //base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}

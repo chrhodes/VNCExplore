12f296c291040e53d71662fa64c7262acfb6c523
Christopher Rhodes <chrhodes@cox.net>
2025-01-24 17:42:15 -0800

    Updates

---
+++ Prism/prism-loading-modules-user-roles-8.1.97/2-prism-loading-modules-user-roles-m2-exercise-files/before/PrismRoles/PrismRoles/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using Microsoft.Practices.Unity;
+using Prism.Modularity;
+using PrismRoles.Views;
+using Prism.Unity;
+
+namespace PrismRoles
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            return new ConfigurationModuleCatalog();
+        }
+    }
+}
